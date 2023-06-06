c13c99231f85e3203bfaced73bdb444bcc6c2ce7
Mike English <mike.english@gmail.com>
2023-06-05 20:04:24 -0400

    Initial implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    for n in 1..=100 {
+        if n % 3 == 0 && n % 5 == 0 {
+            println!("CracklePop");
+            continue;
+        }
+        if n % 3 == 0 {
+            println!("Crackle");
+            continue;
+        }
+        if n % 5 == 0 {
+            println!("Pop");
+            continue;
+        }
+        println!("n: {}", n);
+    }
 }
